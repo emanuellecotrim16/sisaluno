25786dcaed96a52709b818d78b857ff2a40c3fbb
Emanuelle Cotrim <130514378+emanuellecotrim16@users.noreply.github.com>
2023-08-06 17:17:27 -0300

    Add files via upload

---
+++ disciplina/altadisci.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include_once ("header.html"); ?>
+<style>
+  input {
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 200px;
+}
+.alt{
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
+
+<?php
+    require_once('conexao.php');
+
+   $id = $_POST['id'];
+
+   ##sql para selecionar apens um aluno
+   $sql = "SELECT * FROM aluno where id= :id";
+   
+   # junta o sql a conexao do banco
+   $retorno = $conexao->prepare($sql);
+
+   ##diz o paramentro e o tipo  do paramentros
+   $retorno->bindParam(':id',$id, PDO::PARAM_INT);
+
+   #executa a estrutura no banco
+   $retorno->execute();
+
+  #transforma o retorno em array
+   $array_retorno=$retorno->fetch();
+   
+   ##armazena retorno em variaveis
+   $nomedisciplina = $array_retorno['nomedisciplina'];
+   $ch = $array_retorno['ch'];
+   $semestre = $array_retorno['semestre'];
+   $idprofessor = $array_retorno['idprofessor'];
+   
+
+
+?>
+<div class="alt">
+  <form method="POST" action="crudisciplina.php">
+    
+      <p> ID: <br> <input type="text" name="id" id="" value=<?php echo $id?> > </p>
+
+       <p> Nome disciplina: <br>  <input type="text" name="nomedisciplina" id="" required value=""<?php echo $nomedisciplina?> > </p>
+                                                
+       <p> CH: <br> <input type="text" name="ch" id="" value= <?php echo $ch ?> > </p> 
+       
+       <p> SEMESTRE: <br> <input type="text"  name="semestre" id="" value=<?php echo $semestre?>> </p>
+     
+      <p> IDPROFESSOR: <br> <input type="text" name="idprofessor" id="" value=<?php echo $idprofessor ?> > </p>
+     
+      
+    
+        
+        <input type="submit" name="update" value="Alterar">
+  </form>
+  </div>
+  <?php include_once ("footer.html"); ?>
+</body>
+</html>
---
+++ disciplina/listadisci.php
@@ -0,0 +1,82 @@
+<?php 
+ include_once ("header.html"); 
+  require_once('conexao.php');
+   
+  $retorno = $conexao->prepare('SELECT * FROM aluno');
+  $retorno->execute();
+
+?> 
+
+     
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container mt-3">
+       
+  <table class="table table-striped">
+  
+     <th>ID</th>
+                    <th>NOME  DISCIPLINA </th>
+                    <th>CH</th>
+                    <th>SEMESTRE</th>
+                    <th>IDPROFESSOR</th>
+                    
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <?php foreach($retorno->fetchall() as $value) { ?>
+                        <tr>
+                            <td> <?php echo $value['id'] ?>   </td> 
+                            <td> <?php echo $value['nomedisciplina']?>  </td> 
+                            <td> <?php echo $value['ch']?> </td>
+                            <td> <?php echo $value['semestre']?> </td> 
+                            <td> <?php echo $value['idprofessor']?> </td> 
+                            
+
+
+
+
+                            <td>
+                               <form method="POST" action="altadisci.php">
+                                        <input name="id" type="hidden" value="<?php echo $value['id'];?>"/>
+                                        <button name="alterar"  type="submit">Alterar</button>
+                                </form>
+
+                             </td> 
+
+                             <td>
+                               <form method="GET" action="crudisciplina.php">
+                                        <input name="id" type="hidden" value="<?php echo $value['id'];?>"/>
+                                        <button name="excluir"  type="submit">Excluir</button>
+                                </form>
+
+                             </td> 
+
+                             
+                             
+
+
+
+                       
+                      </tr>
+                    <?php  }  ?> 
+                 </tr>
+            </tbody>
+        </table>
+        </body>
+</html>
+<?php         
+   echo "<button class='button button3'><a href='indexd.php'>voltar</a></button>";
+   echo "<button class='button button3'><a href='caddisciplina.php'>novo</a></button>";
+?>
